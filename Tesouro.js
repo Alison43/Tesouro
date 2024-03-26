ec76231a313e905e8e84447e26f8b073ce867f61
Eduardo Alencar <alencareduardo17@gmail.com>
2024-03-25 21:28:14 -0300

    Atualização do JS

--- Tesouro.js
+++ Tesouro.js
@@ -1,67 +1,55 @@
-let escolhas = document.querySelectorAll('.escolhas a');
-
-// console.log(escolhas);
-
-escolhas.forEach((item) => {
-    item.addEventListener('click', clique);
-});
+let escolha1 = document.querySelector('.escolha1 p');
+let escolha2 = document.querySelector('.escolha2 p');
 
 
-function clique(event) {
-    event.preventDefault();
-    let texto = event.target
-    texto.innerText = ilhas.ilha1;
-    console.log(texto);
-}
-
-// const ilhas = {
-//     ilha1: "Ilha dos Piratas",
-//     ilha2: "Ilha dos Mortos",
-//     ilha3: "Ilha do Tesouro",
-//     ilha4: "Baía do Naufrágio",
-//     ilha5: "Enseada dos Contrabandistas",
-//     ilha6: "Ilha dos Amotinas",
-//     ilha7: "Morro do Mosquete"
-// };
-
-// const caminhoilhas = {
-//     ilhaa1: [ilhas.ilha4, ilhas.ilha7],
-//     ilhaa2 : [ilhas.ilha7, ilhas.ilha4],
-//     ilhaa3 : [],
-//     ilhaa4 : [ilhas.ilha7, ilhas.ilha2],
-//     ilhaa5 : [ilhas.ilha1, ilhas.ilha3],
-//     ilhaa6 : [ilhas.ilha5, ilhas.ilha2],
-//     ilhaa7 : [ilhas.ilha1, ilhas.ilha6],
+// function clique(event) {
+//     event.preventDefault();
+//     let texto = event.target
+//     texto.innerText = ilhas.ilha1;
+//     console.log(texto);
 // }
-// console.log(caminhoilhas.ilhaa1);
+
 
 const arrayIlha = ["Ilha dos Piratas", "Ilha dos Mortos", "Ilha do Tesouro", "Baía do Naufrágio", "Enseada dos Contrabandistas", "Ilha dos Amotinas", "Morro do Mosquete"]
 // console.log(arrayIlha[1])
 
-const caminhoilhas = {
-    ilhaa1: [arrayIlha[3], arrayIlha[6]],
-    ilhaa2: [arrayIlha[6], arrayIlha[3]],
-    ilhaa3: [],
-    ilhaa4: [arrayIlha[6], arrayIlha[1]],
-    ilhaa5: [arrayIlha[6], arrayIlha[3]],
-    ilhaa6: [arrayIlha[4], arrayIlha[1]],
-    ilhaa7: [arrayIlha[0], arrayIlha[5]],
-}
+const caminhoilhas = [
+    [0 ,arrayIlha[3], arrayIlha[6]],
+    [1 ,arrayIlha[6], arrayIlha[3]],
+    [2],
+    [3, arrayIlha[6], arrayIlha[1]],
+    [4, arrayIlha[6], arrayIlha[3]],
+    [5, arrayIlha[4], arrayIlha[1]],
+    [6, arrayIlha[0], arrayIlha[5]],
+]
 // console.log(caminhoilhas.ilhaa1);
 
 const textoIlha = document.querySelector('.textotroca');
-let nomeIlha = textoIlha.innerText;
 
-for(let i = 0; i < arrayIlha.length; i++){
-    if (nomeIlha === arrayIlha[i]) {
-        console.log(nomeIlha);
-    } else {
-        console.log("deu ruim");
+
+
+// function algumacoisa() {
+    let nomeIlha = textoIlha.innerText;
+    for(let i = 0; i < arrayIlha.length; i++){
+            if (nomeIlha === arrayIlha[i]) {
+                console.log(arrayIlha[i]);
+    
+                escolha1.innerText = arrayIlha[3]
+                escolha2.innerText = arrayIlha[6]
+                
+            }
     }
+
+// }
+
+
+for (const chave in caminhoilhas) {
+    // console.log(caminhoilhas[chave]);
 }
 
-
-// Object.keys(caminhoilhas).forEach(ilha => {
+// Object.keys(caminhoilhas).forEach((ilha) => { 
+//     console.log(typeof ilha);
+// })
     
 //     const ilhasVizinhas = caminhoilhas[ilha];
   
