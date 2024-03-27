1e8a251029162012495534174d2e5590f5df1e9f
Eduardo Alencar <alencareduardo17@gmail.com>
2024-03-27 16:51:25 -0300

    Atualização geral

--- Tesouro.js
+++ Tesouro.js
@@ -25,7 +25,13 @@ function capturarValor() {
     let respostaUsuario = resposta.value.toLowerCase();
     if(respostaUsuario.includes(arrayResposta[a])) {
         confirmacao = true;
+    } 
+    if(a == 4){
+        if(respostaUsuario.includes('pcdob')){
+        confirmacao = true;
+        }
     }
+    
     resposta.value = "";
     if(confirmacao === true) {
         cadeado.innerText = '✅';
@@ -48,7 +54,7 @@ const caminhoilhas = [
 let imagem = document.querySelector('.trocailha');
 const arrayImagens = [  "Imagens/Ilha dos Piratas.png", //0
                         "Imagens/Ilha dos Mortos.png", //1
-                        "Imagens/Ilha do Tesouro.png", //2
+                        "Imagens/Baleia.png", //2
                         "Imagens/Baía do Naufrágio.png", //3
                         "Imagens/Enseada dos Contrabandistas.png", //4
                         "Imagens/Ilha dos Amotinas.png", //5
@@ -60,15 +66,24 @@ function algo(event, i = 0) {
         let ilha = event.currentTarget.innerText;
         titulo.innerText = ilha;
         let nomeIlha = ilha;
+
         
         // Verificando se a ilha é a mesma definida na tag
         if (nomeIlha === arrayIlha[i]) {
-            // Atribuindo as ilhas à tag <p>
-            escolha1.innerText = caminhoilhas[i][1];
-            escolha2.innerText = caminhoilhas[i][2];
-            pergunta.innerText = arrayPerguntas[i];
 
-    
+            // Atribuindo as ilhas à tag <p>
+            if (nomeIlha === 'Ilha do Tesouro') {
+                titulo.innerText = "Baleia"
+                escolha1.innerText = "Caminho Esquerdo";
+                escolha2.innerText = "Caminho Direito";
+                pergunta.innerText = arrayPerguntas[i];
+            }else{
+                escolha1.innerText = caminhoilhas[i][1];
+                escolha2.innerText = caminhoilhas[i][2];
+                pergunta.innerText = arrayPerguntas[i];
+            }
+
+
             imagem.setAttribute('src', arrayImagens[i]);
 
             a = i;
@@ -78,6 +93,14 @@ function algo(event, i = 0) {
             } 
         }
 
+        if(nomeIlha === "Caminho Direito" || nomeIlha === 'Caminho Esquerdo') {
+            const tela1 = document.querySelector('.tela');
+            const tela2 = document.querySelector('.dnone');
+            tela1.style.display = "none"
+            tela2.style.display = "flex"
+        }
+    }
+    
         // Incrementando o índice para avançar para a próxima ilha
         i++;
 
@@ -85,9 +108,7 @@ function algo(event, i = 0) {
         if (i < arrayIlha.length) {
             algo(event, i);
         }
-    }
 }
-
 
 // Adicionando evento de clique para cada escolha
 escolhas.forEach((item) => {
@@ -96,4 +117,5 @@ escolhas.forEach((item) => {
 
 // Arrays de perguntas e respostas
 const arrayPerguntas = ['Qual a cor da laranja?', 'Qual é a primeira coisa que um homem coloca em sua esposa quando se casam?', 'O que passa pela mão da mulhar mole, e depois fica duro?', 'Por que o kuririn não gosta de IPhone ?', 'Qual o computador mais comunista que existe?', 'Por que a Inglaterra não pode jogar xadrez?','O que tem 50 pernas mais não anda?'];
-const arrayResposta = ['laranja', 'O anel.', 'esmalte', 'android', 'pc do b.', 'rainha', '25 cadeirantes',];
+
+const arrayResposta = ['laranja', 'O anel', 'esmalte', 'android', 'pc do b', 'rainha', '25 cadeirantes'];
